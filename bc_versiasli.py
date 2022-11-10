c3c212d282df8cc44a3d9e19903e44f17ae2ba05
belajar sre <63325373+pyogaswara@users.noreply.github.com>
2022-11-10 23:53:24 +0000

    diberi space di graph height

--- bc_versiasli.py
+++ bc_versiasli.py
@@ -9,7 +9,6 @@ class Category:
         category_title=self.name.center(30,"*")
         self.output.append(category_title)
         for i in range (0,len(self.ledger)):
-            #print((self.ledger[i]["description"]))
             l=str((self.ledger[i]["description"])[:23])
             m=(" "*(23-len(self.ledger[i]["description"])))
             r=(str(('%.2f' % self.ledger[i]["amount"])).rjust(7))
@@ -37,9 +36,7 @@ class Category:
         description=deposit[1]
       else:
         description=""
-        #self.ledger.append([description,amount])
       self.ledger.append({"amount":amount,"description":description})
-      #print(self.ledger)
       
     def withdraw(self,*withdraw):
         withdrawn_amount=withdraw[0]
@@ -50,7 +47,6 @@ class Category:
                 description=withdraw[1]
             else:
                 description=""
-            #self.ledger.append([description,amount])
             self.ledger.append({"amount":amount,"description":description})
             return True
         else:
@@ -67,43 +63,15 @@ class Category:
         if self.check_funds(amount):
             amount=(-1)*amount
             description=f'Transfer to {category.name}'
-            #self.ledger.append([description,amount])
             self.ledger.append({"amount":amount,"description":description})
             destination_description=f'Transfer from {self.name}'
             destination_amount=amount*(-1)
-            #category.ledger.append([destination_description,destination_amount])
             category.ledger.append({"amount":destination_amount,"description":destination_description})
             return True
         else:
             print("insufficient funds")
             return False
 
-              
-
-#Food=Category("Food")
-#Clothing=Category("Clothing")
-
-##transaction1=(1000,"initial deposit")
-#transaction2=(1250,"second deposit")
-#transaction3=(500,"thirddddddddddddd deposit")
-##transaction4=(750,"jajan-jajan")
-#transaction5=(450,"jajan terus")
-#transaction6=(55,"jajan siomay")
-
-
-#Food.deposit(1000,"initial deposit")
-#Food.withdraw(750,"jajan-jajan")
-#Food.transfer(175,"Clothing")
-
-#Clothing.deposit(250,"initial deposit")
-#Food.transfer(25,"Clothing")
-
-#Food.check_funds(100)
-
-#Clothing.check_funds(100)
-#Clothing.transfer(275,"Food")
-#Food.get_balance()
-#Clothing.get_balance()
 
 
 def create_spend_chart(charts):
@@ -132,24 +100,26 @@ def create_spend_chart(charts):
 
     category=charts
     percentage=["100|","90|","80|","70|","60|","50|","40|","30|","20|","10|","0|"]
-    graph_height=[]
+    graph_height=['           ']
     for i in range (0,3):
         length_o=("o"*(int(portion_spend[i])+1)).rjust(11)
         graph_height.append(length_o)
+    print(graph_height)
 
     graph_position=[]
     for i in range(len(percentage)):
         graph_position.append([percentage[i].rjust(4)])
         for j in range(0,len(graph_height)):
+          if j==0:
             graph_position[i].append((graph_height[j][i]))
+          else:
+            graph_position[i].append((graph_height[j][i]).ljust(3,"*"))
+    print(graph_position)
 
     nl='\n'
-    graph=f"{nl.join((str(' '.join(str(y).center(2) for y in x))) for x in graph_position)}"
-
-
+    graph=f"{nl.join((str(''.join(str(y) for y in x))) for x in graph_position)}"
 
     dash=f'{("-"*(3*len(category)+1)).rjust(3*len(category)+5)}'
-
    
     per_element_size=[]
     for i in range (0,len(charts)):
@@ -165,16 +135,17 @@ def create_spend_chart(charts):
 
     legend_list=[]
     for i in range(len(left_empty)):
-        legend_list.append([left_empty[i].rjust(4)])
+        legend_list.append([left_empty[i].rjust(5)])
         for j in range(0,len(category_legend)):
-            legend_list[i].append((category_legend[j][i]))
+            legend_list[i].append((category_legend[j][i]).ljust(3,"*"))
 
     nl='\n'
-    legend=f"{nl.join((str(' '.join(str(y).center(2) for y in x))) for x in legend_list)}"
+    legend=f"{nl.join((str(''.join(str(y) for y in x))) for x in legend_list)}"
+    
 
 
     percentage_charts=f'Percentage spent by category\n'\
-                      f'{graph}\n'\
-                      f'{dash}\n'\
-                      f'{legend}'
+                      f'{graph} \n'\
+                      f'{dash} \n'\
+                      f'{legend} '
     return percentage_charts
--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import bc_versiasli
 from bc_versiasli import create_spend_chart
 
 food = bc_versiasli.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = bc_versiasli.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = bc_versiasli.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+entertainment = bc_versiasli.Category("Entertainment")
+business = bc_versiasli.Category("Business")
+
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
 
 
 print(food)
-print(clothing)
-print(auto)
+print(entertainment)
+print(business)
 
-print(create_spend_chart([food, clothing, auto]))
+print(create_spend_chart([business, food, entertainment]))
+#expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+#print(expected)
